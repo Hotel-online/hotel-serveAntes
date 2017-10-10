5b63b7513ac076584dfdec3f9dbd4c720f0a7ff8
RamirezJosue <xdemobtkx@gmail.com>
2017-10-10 06:59:57 -0500

    completo

--- hotel/admin.py
+++ hotel/admin.py
@@ -1,8 +1,20 @@
 from django.contrib import admin
 from hotel.models import Reservacion
 from hotel.models import CategoriaHabitacion
+from hotel.models import Habitacion
+from hotel.models import DetalleReservacion
+from hotel.models import Forma_de_pago
+from hotel.models import Cliente
+from hotel.models import Venta
+from hotel.models import DetalleVenta
 
 
 # Register your models here.
 admin.site.register(Reservacion)
 admin.site.register(CategoriaHabitacion)
+admin.site.register(Habitacion)
+admin.site.register(DetalleReservacion)
+admin.site.register(Forma_de_pago)
+admin.site.register(Cliente)
+admin.site.register(Venta)
+admin.site.register(DetalleVenta)
--- hotel/models.py
+++ hotel/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from core.models import User, Person
 
 # Create your models here.
 
@@ -16,11 +17,54 @@ class CategoriaHabitacion(models.Model):
         return '%s' % (self.nombre)
 
 
+class Habitacion(models.Model):
+
+    nombre = models.CharField(max_length=60, null=True, blank=True)
+    codigo = models.CharField(max_length=10)
+    detalle = models.TextField(null=True, blank=True)
+    precio_alquiler = models.FloatField(default=0.0)
+    estado = models.CharField(max_length=1)
+    categoriaHabitacion = models.ForeignKey(CategoriaHabitacion)
+
+    class Meta:
+        verbose_name = "Habitacion"
+        verbose_name_plural = "Habitaciones"
+
+    def __str__(self):
+        return '%s' % (self.nombre, self.codigo)
+
+
+class Forma_de_pago(models.Model):
+
+    formaPago = models.CharField(max_length=15)
+
+    class Meta:
+        verbose_name = "Forma_de_pago"
+        verbose_name_plural = "Forma_de_pagos"
+
+    def __str__(self):
+        return '%s' % (self.formaPago)
+
+
+class Cliente(models.Model):
+
+    ruc = models.CharField(max_length=11)
+    person = models.OneToOneField(Person)
+
+    class Meta:
+        verbose_name = "Cliente"
+        verbose_name_plural = "Clientes"
+
+    def __str__(self):
+        return '%s' % (self.ruc)
+
+
 class Reservacion(models.Model):
 
     nro_doc = models.CharField(max_length=15)
     fecha = models.DateTimeField(auto_now_add=True)
     total = models.FloatField(default=0)
+    forma_de_pago = models.ForeignKey(Forma_de_pago)
 
     class Meta:
         verbose_name = "Reservacion"
@@ -28,3 +72,47 @@ class Reservacion(models.Model):
 
     def __str__(self):
         return '%s' % (self.nro_doc)
+
+
+class DetalleReservacion(models.Model):
+
+    fecha = models.DateTimeField(auto_now_add=True)
+    habitacionesR = models.IntegerField()
+    precio = models.FloatField(default=0)
+    habitacion = models.ForeignKey(Habitacion)
+    reservacion = models.ForeignKey(Reservacion)
+
+    class Meta:
+        verbose_name = "DetalleReservacion"
+        verbose_name_plural = "DetalleReservaciones"
+
+    def __str__(self):
+        return '%s (%s)' % (self.habitacionesR)
+
+
+class Venta(models.Model):
+
+    nro_doc = models.CharField(max_length=15)
+    numeroReservacion = models.IntegerField()
+    vendedor = models.ForeignKey(User)
+    cliente = models.ForeignKey(Cliente, blank=True, null=True)
+
+    class Meta:
+        verbose_name = "Venta"
+        verbose_name_plural = "Ventas"
+
+    def __str__(self):
+        return '%s' % (self.nro_doc)
+
+
+class DetalleVenta(models.Model):
+
+    detalle = models.TextField(null=True, blank=True)
+    venta = models.ForeignKey(Venta)
+
+    class Meta:
+        verbose_name = "DetalleVenta"
+        verbose_name_plural = "DetalleVentas"
+
+    def __str__(self):
+        return '%s' % (self.detalle)
